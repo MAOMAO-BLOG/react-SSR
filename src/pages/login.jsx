76e098aab3b5a91492737f105828d97277536575
hxx <2281313697@qq.com>
2023-03-17 19:27:28 +0800

    '123'

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,7 +5,7 @@ import { login } from '@/store/actions/login';
 function mapDispatch(dispatch) {
     return {
         async onSubmit({ loginId, loginPwd }) {
-            
+            //巨大法律空白地方可能
             return await dispatch(login(loginId, loginPwd));
                 
         }
